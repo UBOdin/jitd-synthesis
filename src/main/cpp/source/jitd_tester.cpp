6f6c746e7c93e7dd96fa55181891d617203bf2e1
carlnues@buffalo.edu <carlnues@buffalo.edu>
2019-10-25 16:58:55 -0400

    Simple insert-get harness test

--- src/main/cpp/source/jitd_tester.cpp
+++ src/main/cpp/source/jitd_tester.cpp
@@ -414,6 +414,7 @@ int main(int argc, char **argv)
 {
 
   jitd_harness();
+  return 0;
 
   std::istream *src;
   int i, t = 0;
@@ -456,15 +457,34 @@ int jitd_harness() {
 	Record r;
 	int ms;
 
+	int testarray[] = {41, 6, 7, 19, 51, 95, 94, 93, 88, 5};
+	bool observed;
+	bool expected;
+
 	printf("Starting\n");
 
 	gettimeofday(&start, NULL);
-	r.key = value_array[0];
+	r.key = 888; //value_array[0];
 	r.value = (Value)0xDEADBEEF;
 
 	data.push_back(r);
 	jitd = std::shared_ptr<JITD>(new JITD(new ArrayNode(data)));
 
+/*
+	bool result;
+
+	printf("debug 0\n");
+	jitd->print_debug();
+	//jitd->insert(data);
+	Record r2;
+	printf("debug 1\n");
+	jitd->print_debug();
+	result = jitd->get(878, r2);
+	printf("Result:  %d\n", result);
+	return 0;
+*/
+
+/*
 	for (int i = 0; i < 300; i++) {
 		printf("%d\n", i);
 		r.key = value_array[i + 1];
@@ -473,12 +493,41 @@ int jitd_harness() {
 		ms = delay_array[i]; // * 1000;
 //		std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 	}
+*/
+
+	printf("Inserting 10 data into jitds\n");
+	for (int i = 0; i < 10; i++) {
+		r.key = testarray[i];
+		data.pop_back();
+		data.push_back(r);
+		jitd->insert(data);
+	}
+
+	printf("Verifying inserted data\n");
+	for (int i = 0; i < 100; i++) {
+		// Check whether target value _was_ inserted:
+		expected = false;
+		for (int j = 0; j < 10; j++) {
+			if (testarray[j] == i) {
+				expected = true;
+				break;
+			}
+		}
+		// Check whether target value is reported by jitds:
+		observed = jitd->get(i, r);
+		if (expected != observed) {
+			printf("jitds test failure:  expected = %d; observed = %d on item %d\n", expected, observed, i);
+			_exit(1);
+		}
+
+	}
+	printf("Passed jitds harness basic test\n");
 
 	gettimeofday(&end, NULL);
 
 	std::cout << "Init JITD: " << total_time(start, end) << " us" << std::endl;
 	printf("End\n");
-	_exit(0);
+	return 0;
 
 /*
 	opnode foo;
