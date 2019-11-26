799c63b3ac1fba850ac9582481405d67c9acc0af
carlnues@buffalo.edu <carlnues@buffalo.edu>
2019-11-26 07:57:04 -0500

    script tweak

--- batch_graphs.sh
+++ batch_graphs.sh
@@ -17,7 +17,7 @@ for e in $runs; do
 	mv latency_walltime.pdf graphs/latency_walltime_sqlite_$e.pdf
 	mv latency_rowcount.pdf graphs/latency_rowcount_sqlite_$e.pdf
 
-	python jitd_diff.py output/sqlite_$e.txt output/jitd_$e.txt script
+	python jitd_diff.py output/jitd_$e.txt output/sqlite_$e.txt script
 	mv latency_difference.pdf graphs/latency_difference_$e.pdf
 
 done
