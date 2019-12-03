f35f91fcb71ea171b69877ddd1f91ccf403026c0
carlnues@buffalo.edu <carlnues@buffalo.edu>
2019-12-03 08:30:36 -0500

    tweak

--- batch_graphs.sh
+++ batch_graphs.sh
@@ -6,14 +6,14 @@ runs="0 1 2 3" # runs
 for e in $runs; do
 
 	echo "\nRunning instance $e\n"
-	python jitd_tdf.py output/jitd_$e.txt script
+	python jitd_latency.py output/jitd_$e.txt script
 	mv latency_frequency.pdf graphs/latency_frequency_jitd_$e.pdf
 	mv latency_walltime.pdf graphs/latency_walltime_jitd_$e.pdf
 	mv latency_rowcount.pdf graphs/latency_rowcount_jitd_$e.pdf
 	mv latency_key.pdf graphs/latency_key_jitd_$e.pdf
 	# (skip cdf)
 
-	python jitd_tdf.py output/sqlite_$e.txt script
+	python jitd_latency.py output/sqlite_$e.txt script
 	mv latency_frequency.pdf graphs/latency_frequency_sqlite_$e.pdf
 	mv latency_walltime.pdf graphs/latency_walltime_sqlite_$e.pdf
 	mv latency_rowcount.pdf graphs/latency_rowcount_sqlite_$e.pdf
