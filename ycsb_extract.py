d192d8d24e548570b7434da225e0c45566f97aa8
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-01-24 02:44:06 -0500

    ycsb script:  generates initialization and benchmark operations with YCSB column data

--- ycsb_extract.py
+++ ycsb_extract.py
@@ -4,6 +4,11 @@ import os
 import json
 
 
+# Fill data for C++ column fields:
+fill900 = "." * 900
+fill1000 = "." * 1000
+
+
 def sort_key(input_list):
 
 	return input_list[0]
@@ -103,11 +108,14 @@ def get_data(file_name):
 			#end_if
 
 			field_key = logline[index + 5:index + 6]
-			#parseline_list.append(field_key)
-
 			field_value = logline[index + 7:index + 107]
-			field_value = field_value.replace(" ", "_")
-			#parseline_list.append(field_value)
+
+			# Escape backslash and quote characters (magic meaning in C strings):
+			field_value = field_value.replace("\\", "\\\\")
+			field_value = field_value.replace("\"", "\\\"")
+			# Escape question marks (double ?? is preprocessor trigraph alert):
+			field_value = field_value.replace("?", "\\?")
+			#field_value = field_value.replace("\'", "\\\'")
 
 			presort_list.append([field_key, field_value])
 
@@ -120,18 +128,12 @@ def get_data(file_name):
 
 		#end_while
 
-		#print("start sort")
-
 		presort_list = sorted(presort_list, key = sort_key)
-
-		#print("end sort")
 
 		for e in presort_list:
 			parseline_list.append(e[0])
 			parseline_list.append(e[1])
 		#end_for
-
-		#print("end append")
 
 		parseline_list_list.append(parseline_list)
 
@@ -194,13 +196,18 @@ def process_initialize_benchmark_pair(workload):
 
 	# workload = ""
 	# -----
-	initialize_list_list = []
-	benchmark_list_list = []
 	benchmark_file = "" # file obj
+	initialize_list_list = []
+	initialize_list = []
+	benchmark_list_list = []
+	benchmark_list = []
+	listlen = 0
 	output_line = ""
 	operation = ""
 	key = ""
 	rows = ""
+	field = ""
+	val = ""
 
 	initialize_list_list = process_workload("initialize_" + workload)
 	benchmark_list_list = process_workload("benchmark_" + workload)
@@ -212,12 +219,19 @@ def process_initialize_benchmark_pair(workload):
 	benchmark_file.write("struct operation_node initialize_array[] = {\n")
 	for initialize_list in initialize_list_list:
 		operation = initialize_list[0]
+		assert (len(initialize_list) == 22), "Unexpected initialize field length"
 		if (operation != "INSERT"):
 			print("Unsupported initialize operation")
 			sys.exit(1)
 		#end_if
 		key = initialize_list[1]
-		benchmark_file.write("\t{ .type = INSERT, .time = 0.0, .key = " + key + "},\n")
+		val = ""
+		for i in range(2, 22, 2):
+			# Fields should be 0, 1, ... 9:
+			assert(int(initialize_list[i]) == (i - 2) / 2), "val:  %s" % (initialize_list[i])
+			val += initialize_list[i + 1]
+		#end_for
+		benchmark_file.write("\t{ .type = INSERT, .time = 0.0, .key = " + key + ", .rows = 0, .field = 0, .val = \"" + val + "\" },\n")
 	#end_for
 	benchmark_file.write("\t{ .type = STOP },\n")
 	benchmark_file.write("};\n")
@@ -225,17 +239,35 @@ def process_initialize_benchmark_pair(workload):
 	benchmark_file.write("struct operation_node benchmark_array[] = {\n")
 	for benchmark_list in benchmark_list_list:
 		operation = benchmark_list[0]
+		key = benchmark_list[1]
+		listlen = len(benchmark_list)
 		if (operation == "READ"):
+			assert (listlen == 2), "Unexpected read field length"
 			operation = "SELECT"
-			rows = ", .rows = 0"
-		elif ((operation == "UPDATE") or (operation == "INSERT") or (operation == "SCAN")):
-			rows = ""
+			field = "-1" # Magic value:  field is N/A
+			val = fill1000 # in C++ 14, initializer size must exactly match declared size
+		elif (operation == "UPDATE"):
+			assert (listlen == 4), "Unexpected update field length"
+			field = benchmark_list[2]
+			val = benchmark_list[3] + fill900 # As above match size...
+		elif (operation == "INSERT"):
+			assert (listlen == 22), "Unexpected insert field length"
+			field = "10" # Magic value:  all 10 (0-9), not #10
+			val = ""
+			for i in range(2, 22, 2):
+				# Fields should be 0, 1, ... 9:
+				assert(int(initialize_list[i]) == (i - 2) / 2), "val:  %s" % (initialize_list[i])
+				val += initialize_list[i + 1]
+			#end_for
+		elif (operation == "SCAN"):
+			assert (listlen == 2), "Unexpected scan field length"
+			field = "-1" # Magic value:  field is N/A
+			val = fill1000 # As above, match size...
 		else:
 			print("Unsupported benchmark operation")
 			sys.exit(1)
 		#end_if
-		key = benchmark_list[1]
-		benchmark_file.write("\t{ .type = " + operation + ", .time = 0.0, .key = " + key + rows + "},\n")
+		benchmark_file.write("\t{ .type = " + operation + ", .time = 0.0, .key = " + key + ", .rows = 0, .field = " + field + ", .val = \"" + val + "\" },\n")
 	#end_for
 	benchmark_file.write("\t{ .type = STOP },\n")
 	benchmark_file.write("};\n")
