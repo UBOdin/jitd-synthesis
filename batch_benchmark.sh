ac55a3c49b0ed10f59fe3ebec065fbdae348693f
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-05-26 18:53:51 -0400

    Reworked benchmark script to use makefile with parameterized jitd builds

--- batch_benchmark.sh
+++ batch_benchmark.sh
@@ -1,42 +1,53 @@
 
 echo "Running batch"
 
-storage_list="asal_default asal_aligned map uom"
+storage_list="asal map uom"
 keymax_list="30000 3"
 
 #make clean
 
 for storage in $storage_list; do
 
-	# Paranoid:  Force rebuilds of jitd (source may not have changed, but preprocessor -D may have):
-	rm jitd_*.o
-
-	make clean
-	make jitd_storage_${storage}
-	if [ "$?" != "0" ]; then
-		echo "Error on build"
-		exit 1
+	if [ ${storage:0:1} = "a" ]; then
+		threshhold_list="2 2000"
+		alloc_list="default aligned"
+		thread_list="pthread intel"
+	else
+		# Dummy values for non-jitd:
+		threshhold_list="X"
+		alloc_list="Y"
+		thread_list="Z"
 	fi
-	echo "Clean build on ${storage}"
 
-	for keymax in $keymax_list; do
+	for alloc in $alloc_list; do
 
-		if [ ${storage:0:1} = "a" ]; then
-			threshhold_list="2 2000"
-		else
-			threshhold_list="X"  # dummy -- N/A for non-jitd
-		fi
+		for thread in $thread_list; do
 
-		for threshhold in $threshhold_list; do
-
-			printf "\nRunning ${storage} instance with ${keymax} keys and crack threshhold of ${threshhold}\n"
-			sleep 10
-			./jitd_harness.exe $threshhold $keymax
+			# Rebuild for each new storage type (where the binary changes) for paranoia
+			make clean
+			make jitd_storage_${storage} alloc=${alloc} thread=${thread}  # N.b. alloc and thread parameters N/A for jitd -- probably should pull
 			if [ "$?" != "0" ]; then
-				echo "Error on JITD"
+				echo "Error on build"
 				exit 1
 			fi
-			mv output_data.txt ${storage}_${threshhold}_${keymax}_output_data.txt
+			echo "Clean build on ${storage}"
+
+			for keymax in $keymax_list; do
+
+				for threshhold in $threshhold_list; do
+
+					printf "\nRunning ${storage} instance with ${alloc} allocator, ${thread} threading, ${keymax} keys and crack threshhold of ${threshhold}\n"
+					sleep 10
+					./jitd_harness.exe $threshhold $keymax
+					if [ "$?" != "0" ]; then
+						echo "Error on JITD"
+						exit 1
+					fi
+					mv output_data.txt ${storage}_${thread}_${threshhold}_${keymax}_output_data.txt
+
+				done
+
+			done
 
 		done
 
