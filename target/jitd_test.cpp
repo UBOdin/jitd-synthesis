615c564b74a8adc8efb6c4d0c72d5591290d1227
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-07-01 18:11:17 -0400

    jitd:  added maintenance tracking of child nodes

--- target/jitd_test.cpp
+++ target/jitd_test.cpp
@@ -19,6 +19,7 @@ long unsigned int sticks, diffticks;
 int delta_count = 0;
 int ticks_count = 0;
 int maint_count = 0;
+int maint_block_start = 0;
 int maint_type;
 struct ticks_node ticks_array[TICKS_SIZE];
 struct maint_node maint_array[MAINT_SIZE];
@@ -39,6 +40,12 @@ inline void view_end() {
 	}
 	ticks_array[ticks_count].delta[delta_count] = diffticks;
 	delta_count++;
+	if (delta_count == 2) {
+		maint_block_start = maint_count;  // Record the start index of viewAdd calls for a maintenence operations (either transform or mutator)
+
+printf("Start block:  %d\n", maint_count);
+
+	}
 	if (delta_count == 3) {
 		delta_count = 0;
 		ticks_array[ticks_count].id = ticks_count;
@@ -64,27 +71,64 @@ inline void record_maintenance(std::shared_ptr<JITDNode>* node_handle, int rw) {
 	maint_array[maint_count].rw = rw;
 	maint_array[maint_count].maint_type = maint_type;
 	maint_array[maint_count].node_type = (*node_handle)->type;
-	maint_array[maint_count].node_id = (unsigned long)node_handle;
-	maint_array[maint_count].parent = 0;
+	maint_array[maint_count].node_self = (unsigned long)node_handle;
+//	maint_array[maint_count].parent = (unsigned long)((*node_handle)->parent);
 
 	JITDNodeType node_type = (*node_handle)->type;
 
-	if (node_type == JITD_NODE_BTree) {
+	if (node_type == JITD_NODE_DeleteSingleton) {
+		auto deletesingleton_node_handle = (std::shared_ptr<DeleteSingletonNode>*)node_handle;
+		auto child_handle = &((*deletesingleton_node_handle)->node);
+		maint_array[maint_count].node_child = (unsigned long)child_handle;
+	} else if (node_type == JITD_NODE_DeleteElements) {
+		auto deleteelements_node_handle = (std::shared_ptr<DeleteElementsNode>*)node_handle;
+		auto child_handle = &((*deleteelements_node_handle)->node);
+		maint_array[maint_count].node_child = (unsigned long)child_handle;
+	} else if (node_type == JITD_NODE_BTree) {
 		auto btree_node_handle = (std::shared_ptr<BTreeNode>*)node_handle;
-		auto lhs_handle = &((*btree_node_handle)->lhs);
-		auto rhs_handle = &((*btree_node_handle)->rhs);
-		maint_array[maint_count].lhs = (unsigned long)lhs_handle;
-		maint_array[maint_count].rhs = (unsigned long)rhs_handle;
-	}
-	if (node_type == JITD_NODE_Concat) {
+		auto left_handle = &((*btree_node_handle)->lhs);
+		auto right_handle = &((*btree_node_handle)->rhs);
+		maint_array[maint_count].node_left = (unsigned long)left_handle;
+		maint_array[maint_count].node_right = (unsigned long)right_handle;
+	} else if (node_type == JITD_NODE_Concat) {
 		auto concat_node_handle = (std::shared_ptr<ConcatNode>*)node_handle;
-		auto lhs_handle = &((*concat_node_handle)->lhs);
-		auto rhs_handle = &((*concat_node_handle)->rhs);
-		maint_array[maint_count].lhs = (unsigned long)lhs_handle;
-		maint_array[maint_count].rhs = (unsigned long)rhs_handle;
+		auto left_handle = &((*concat_node_handle)->lhs);
+		auto right_handle = &((*concat_node_handle)->rhs);
+		maint_array[maint_count].node_left = (unsigned long)left_handle;
+		maint_array[maint_count].node_right = (unsigned long)right_handle;
 	}
 
 	maint_count++;
+
+	return;
+
+}
+
+inline void record_parent(std::shared_ptr<JITDNode>* node_handle,std::shared_ptr<JITDNode>* parent) {
+
+	int lookback_index;
+
+	for (lookback_index = maint_block_start; lookback_index < maint_count; lookback_index++) {
+
+		if (maint_array[lookback_index].node_self == (unsigned long)node_handle) {
+
+if (maint_array[lookback_index].node_parent != 0) {
+	printf("Error:  double match\n");
+	_exit(1);
+}
+
+			maint_array[lookback_index].node_parent = (unsigned long)parent;
+
+if (parent == NULL) {
+	maint_array[lookback_index].node_parent = 12345;  // Magic value for testing
+}
+
+			return;
+		}
+	}
+
+printf("Error:  No match\n");
+_exit(1);
 
 	return;
 
@@ -3204,6 +3248,9 @@ std::shared_ptr<JITDNode> * JITD::getParentMap(std::shared_ptr<JITDNode> * &targ
 */
 void JITD::setParent(std::shared_ptr<JITDNode>* node_handle,std::shared_ptr<JITDNode>* parent)
 {
+
+record_parent(node_handle, parent);
+
     std::shared_ptr<JITDNode> node_ptr;
     #ifdef ATOMIC_LOAD
     node_ptr = std::atomic_load((node_handle));
