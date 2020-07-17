a2ea77cff3f4747ca2686655d135b3679da23584
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-07-17 10:20:37 -0400

    added build script for toaster generated code

---
+++ toaster_harness/build_hpp.sh
@@ -0,0 +1,6 @@
+
+#../../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp
+#../../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp -O3 --FEXPRESSIVE-TLQS --batch
+../../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp -O3 --FEXPRESSIVE-TLQS #--batch
+#../../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l m3 -o toaster_maintenance.m3 -O3 --FEXPRESSIVE-TLQS #--batch
+
