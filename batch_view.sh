7cee9cc24bd0d10339e4e6cbfee6b5a7a3611729
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-08 11:14:10 -0400

    view script:  running set-based transform selection

--- batch_view.sh
+++ batch_view.sh
@@ -3,12 +3,12 @@ echo "Starting view batch"
 
 #workload_list="a b c d e f"
 workload_list="a b c d f"
-#workload_list="f"
+#workload_list="a f"
 
 run_list="0 1 2 3 4 5 6 7 8 9"
 
-crack_size="100"
-prepopulate="30000000"
+crack_size="1000"
+prepopulate="30000000"  # A very big number; ensure all keys are pre-populated
 
 echo "Running with CRACKSIZE ${crack_size}"
 
@@ -24,9 +24,17 @@ for workload in $workload_list; do
 		echo "Running run ${run}"
 
 		sleep 2
-		./replay_jitd.exe $crack_size $prepopulate 100
+		./replay_set.exe $crack_size $prepopulate 100
 		if [ "$?" != "0" ]; then
-			echo "Error on jitd replay"
+			echo "Error on set replay"
+			exit 1
+		fi
+		mv output_view_performance.txt view_results/set_view_performance_${workload}_${run}.txt
+		mv output_data.txt view_results/set_data_performance_${workload}_${run}.txt
+		sleep 2
+		./replay_view.exe $crack_size $prepopulate 100
+		if [ "$?" != "0" ]; then
+			echo "Error on view replay"
 			exit 1
 		fi
 		mv output_view_performance.txt view_results/jitd_view_performance_${workload}_${run}.txt
