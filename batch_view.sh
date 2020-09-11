ddbd9f6ccd63e3504ccbd3d5f4d1f1e4258b8b6d
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-10 22:43:46 -0400

    benchmark script:  automated varying keyspace runs

--- batch_view.sh
+++ batch_view.sh
@@ -1,58 +1,95 @@
 
 echo "Starting view batch"
 
+#keyspace_list="4 5 6"
+keyspace_list="5 6"
+
 #workload_list="a b c d e f"
 workload_list="a b c d f"
 #workload_list="a f"
 
 run_list="0 1 2 3 4 5 6 7 8 9"
 
-crack_size="1000"
+crack_size="1000"  # Will change
 prepopulate="30000000"  # A very big number; ensure all keys are pre-populated
 
-echo "Running with CRACKSIZE ${crack_size}"
+sleep_time="100"
 
-for workload in $workload_list; do
+make build_replay_dbop
+if [ "$?" != "0" ]; then
+	echo "Error on dbop build"
+	exit 1
+fi
+make build_replay_node
+if [ "$?" != "0" ]; then
+	echo "Error on node build"
+	exit 1
+fi
+	
+for keyspace in $keyspace_list; do
 
-	echo "\nRunning workload ${workload}"
+	if [ "$keyspace" = "4" ];then
+		crack_size="1000"
+	elif [ "$keyspace" = "5" ]; then
+		crack_size="10000"
+	elif [ "$keyspace" = "6" ]; then
+		crack_size="100000"
+	else
+		printf "Unrecognized keyspace\n"
+		exit 1
+	fi
 
-	cp ycsb_benchmark/ycsb_tab_initialize_${workload}.tsv initialize_data.txt
-	cp ycsb_benchmark/ycsb_tab_benchmark_${workload}.tsv benchmark_data.txt
+	echo "\nRunning with CRACKSIZE ${crack_size}\n"
 
-	for run in $run_list; do
+	cp YCSB_RAW_10e${keyspace}/* ycsb_benchmark
 
-		echo "Running run ${run}"
+	for workload in $workload_list; do
 
-		sleep 2
-		./replay_set.exe $crack_size $prepopulate 100
-		if [ "$?" != "0" ]; then
-			echo "Error on set replay"
-			exit 1
-		fi
-		mv output_view_performance.txt view_results/set_view_performance_${workload}_${run}.txt
-		mv output_data.txt view_results/set_data_performance_${workload}_${run}.txt
-		sleep 2
-		./replay_view.exe $crack_size $prepopulate 100
-		if [ "$?" != "0" ]; then
-			echo "Error on view replay"
-			exit 1
-		fi
-		mv output_view_performance.txt view_results/jitd_view_performance_${workload}_${run}.txt
-		mv output_data.txt view_results/jitd_data_performance_${workload}_${run}.txt
-		sleep 2
-		./replay_dbt.exe $crack_size $prepopulate 100
-		if [ "$?" != "0" ]; then
-			echo "Error on dbt replay"
-			exit 1
-		fi
-		mv output_view_performance.txt view_results/dbt_view_performance_${workload}_${run}.txt
-		mv output_data.txt view_results/dbt_data_performance_${workload}_${run}.txt
+		echo "\nRunning workload ${workload}\n"
+
+		cp ycsb_benchmark/ycsb_tab_initialize_${workload}.tsv initialize_data.txt
+		cp ycsb_benchmark/ycsb_tab_benchmark_${workload}.tsv benchmark_data.txt
+
+		for run in $run_list; do
+
+			echo "Running run ${run}"
+
+			sleep 2
+			./replay_node_set.exe $crack_size $prepopulate $sleep_time
+			if [ "$?" != "0" ]; then
+				echo "Error on set replay"
+				exit 1
+			fi
+			mv output_view_performance.txt view_results/set_view_performance_${workload}_${run}.txt
+			mv output_data.txt view_results/set_data_performance_${workload}_${run}.txt
+			sleep 2
+			./replay_node_view.exe $crack_size $prepopulate $sleep_time
+			if [ "$?" != "0" ]; then
+				echo "Error on view replay"
+				exit 1
+			fi
+			mv output_view_performance.txt view_results/jitd_view_performance_${workload}_${run}.txt
+			mv output_data.txt view_results/jitd_data_performance_${workload}_${run}.txt
+			sleep 2
+			./replay_node_dbt.exe $crack_size $prepopulate $sleep_time
+			if [ "$?" != "0" ]; then
+				echo "Error on dbt replay"
+				exit 1
+			fi
+			mv output_view_performance.txt view_results/dbt_view_performance_${workload}_${run}.txt
+			mv output_data.txt view_results/dbt_data_performance_${workload}_${run}.txt
+
+		done
 
 	done
 
+	rm -rf view_results_10e${keyspace}
+	mkdir view_results_10e${keyspace}
+	mv view_results/* view_results_10e${keyspace}
+
 done
 
-echo "\nRunning with CRACKSIZE ${crack_size}"
+#echo "\nRunning with CRACKSIZE ${crack_size}"
 
 echo "\nFinished"
 
