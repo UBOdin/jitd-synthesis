0195b390f234e4b580f324e3eadd0aa7c0240bd9
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-14 17:59:51 -0400

    view graph script:  ditto below

--- batch_graphs.sh
+++ batch_graphs.sh
@@ -2,13 +2,18 @@
 #run_list="3 4 5 6"
 run_list="4 5 6"
 
+operations="3"
+
+#machine="SPINNERS"
+machine="MJOLNIR"
+
 echo "Generating graphs"
 
 for run in $run_list; do
 
 	echo "\nDoing run with 10e${run} keys\n"
 
-	cp MJOLNIR_view_results_10e${run}/* view_results
+	cp ${machine}_view_results_10e${run}k_10e${operations}o/* view_results
 	python data_process.py
 	if [ "$?" != "0" ];then
 		echo "Error on python data graph script"
@@ -19,7 +24,10 @@ for run in $run_list; do
 		echo "Error on python view graph script"
 		exit 1
 	fi
-	cp view_graphs/* MJOLNIR_view_graphs_10e${run}
+
+	rm -rf ${machine}_view_graphs_10e${run}k_10e${operations}o
+	mkdir ${machine}_view_graphs_10e${run}k_10e${operations}o
+	mv view_graphs/* ${machine}_view_graphs_10e${run}k_10e${operations}o
 	if [ "$?" != "0" ];then
 		echo "Error on exporting data"
 		exit 1
