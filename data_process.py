5f7feeddbad3bf150d864c158939be1beea04ba6
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-16 16:50:49 -0400

    dbop graph script:  added naive selection plots

--- data_process.py
+++ data_process.py
@@ -83,12 +83,14 @@ def graph_boxplot(workload):
 	input_file_name = ""
 	results_list_list = []
 
-	fig_list, ax_list = plt.subplots(1, 3, sharex = True)
-	ax2_list = [ax_list[0].twinx(), ax_list[1].twinx(), ax_list[2].twinx()]
+	fig_list, ax_list = plt.subplots(1, 4, sharex = True)
+	ax2_list = [ax_list[0].twinx(), ax_list[1].twinx(), ax_list[2].twinx(), ax_list[3].twinx()]
 
 	fig_list.set_size_inches(22, 12)
 
 	index_list = []
+	naive_latency_list = []
+	naive_total_list = []
 	set_latency_list = []
 	set_total_list = []
 	jitd_latency_list = []
@@ -100,6 +102,12 @@ def graph_boxplot(workload):
 
 		print(i)
 		index_list.append(i + 1)
+
+		input_file_name = "view_results/naive_data_performance_" + workload + "_" + str(i) + ".txt"
+		results_list_list = []
+		process_loglines(input_file_name, results_list_list)
+		naive_latency_list.append(results_list_list[1])
+		naive_total_list.append(results_list_list[2])
 
 		input_file_name = "view_results/set_data_performance_" + workload + "_" + str(i) + ".txt"
 		results_list_list = []
@@ -133,7 +141,7 @@ def graph_boxplot(workload):
 	ax_list[0].set_ylabel("Database Operation Latency ($ms$)", fontsize = 14, fontweight = "bold")
 	ax_list[0].axis([0, 11, 0, 200])
 
-	ax2_list[0].plot(index_list, set_total_list, marker = "o", color = "blue", label = "Set Total time (right axis)")
+	ax2_list[0].plot(index_list, set_total_list, marker = "o", color = "blue", label = "Naive Total time (right axis)")
 	ax2_list[0].axis([0, 11, 0, 70000])
 	ax2_list[0].legend(loc = "upper right")
 
@@ -153,7 +161,7 @@ def graph_boxplot(workload):
 	ax_list[1].set_xlabel("JITD View Run #", fontsize = 14, fontweight = "bold")
 	ax_list[1].axis([0, 11, 0, 200])
 
-	ax2_list[1].plot(index_list, jitd_total_list, marker = "o", color = "red", label = "JITD View Total time (right axis)")
+	ax2_list[1].plot(index_list, jitd_total_list, marker = "o", color = "red", label = "Set View Total time (right axis)")
 	ax2_list[1].axis([0, 11, 0, 70000])
 	ax2_list[1].legend(loc = "upper right")
 
@@ -166,21 +174,47 @@ def graph_boxplot(workload):
 	ax2_list[1].set_yticklabels(y_labels)
 
 
-	bp = ax_list[2].boxplot(dbt_latency_list)
+
+
+	bp = ax_list[2].boxplot(jitd_latency_list)
+
+	for flier in bp['fliers']:
+		flier.set(marker='.', color='#e7298a', alpha=0.5)
+	#end_for
 
 	ax_list[2].set_title("Database Operation Latency (YCSB " + workload.upper() + ")", fontsize = 14, fontweight = "bold")
-	ax_list[2].set_xlabel("DBT View Run #", fontsize = 14, fontweight = "bold")
+	ax_list[2].set_xlabel("JITD View Run #", fontsize = 14, fontweight = "bold")
 	ax_list[2].axis([0, 11, 0, 200])
 
-	ax2_list[2].plot(index_list, dbt_total_list, marker = "o", color = "green", label = "DBT View Total time (right axis)")
-	ax2_list[2].set_ylabel("Total View Operation Latency", fontsize = 14, fontweight = "bold")
+	ax2_list[2].plot(index_list, jitd_total_list, marker = "o", color = "red", label = "JITD View Total time (right axis)")
 	ax2_list[2].axis([0, 11, 0, 70000])
 	ax2_list[2].legend(loc = "upper right")
 
-	# Remove LH Y tick labels on RH subplot:
+	# Remove both LH and RH labels from middle subplot:
 	y_labels = ax_list[2].get_yticklabels()
 	y_labels = [ "" for e in y_labels]
 	ax_list[2].set_yticklabels(y_labels)
+	y_labels = ax2_list[1].get_yticklabels()
+	y_labels = [ "" for e in y_labels]
+	ax2_list[2].set_yticklabels(y_labels)
+
+
+
+	bp = ax_list[3].boxplot(dbt_latency_list)
+
+	ax_list[3].set_title("Database Operation Latency (YCSB " + workload.upper() + ")", fontsize = 14, fontweight = "bold")
+	ax_list[3].set_xlabel("DBT View Run #", fontsize = 14, fontweight = "bold")
+	ax_list[3].axis([0, 11, 0, 200])
+
+	ax2_list[3].plot(index_list, dbt_total_list, marker = "o", color = "green", label = "DBT View Total time (right axis)")
+	ax2_list[3].set_ylabel("Total View Operation Latency", fontsize = 14, fontweight = "bold")
+	ax2_list[3].axis([0, 11, 0, 70000])
+	ax2_list[3].legend(loc = "upper right")
+
+	# Remove LH Y tick labels on RH subplot:
+	y_labels = ax_list[3].get_yticklabels()
+	y_labels = [ "" for e in y_labels]
+	ax_list[3].set_yticklabels(y_labels)
 
 
 
