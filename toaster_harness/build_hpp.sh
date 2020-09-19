ac6c4d4a9d65959384676840f06f519997c6c459
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-19 15:36:00 -0400

    toaster build script:  builds both original and new "classic" outputs

--- toaster_harness/build_hpp.sh
+++ toaster_harness/build_hpp.sh
@@ -2,5 +2,6 @@
 #/../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp
 #/../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp -O3 --FEXPRESSIVE-TLQS --batch
 ../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o toaster_maintenance.hpp -O3 --FEXPRESSIVE-TLQS #--batch
+../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l cpp -o classic_maintenance.hpp -O3 --depth 1 --FEXPRESSIVE-TLQS #--batch
 #/../../dbtoaster/bin/dbtoaster toaster_maintenance.sql -l m3 -o toaster_maintenance.m3 -O3 --FEXPRESSIVE-TLQS #--batch
 
