ccc1a9dfd35029fb11b0bd041333b761f9d93d38
carlnues@buffalo.edu <carlnues@buffalo.edu>
2020-09-21 17:08:48 -0400

    graph processing script:  added sanity check

--- batch_graphs.sh
+++ batch_graphs.sh
@@ -1,6 +1,8 @@
 
 #run_list="3 4 5 6"
-run_list="4 5 6"
+#run_list="4 5 6"
+run_list="5 6 7"
+#run_list="7"
 
 operations="3"
 
@@ -14,6 +16,10 @@ for run in $run_list; do
 	echo "\nDoing run with 10e${run} keys\n"
 
 	cp ${machine}_view_results_10e${run}k_10e${operations}o/* view_results
+	if [ "$?" != "0" ];then
+		echo "Error on importing data"
+		exit 1
+	fi
 	python data_process.py
 	if [ "$?" != "0" ];then
 		echo "Error on python data graph script"
