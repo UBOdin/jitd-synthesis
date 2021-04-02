09137a365be0eb6e0330d0c42c8d6a0382e2760f
carlnues@buffalo.edu <carlnues@buffalo.edu>
2021-04-02 14:01:32 -0400

    Edited Readme.  Toplevel script downloads and unpacks tracefiles.

--- treetoaster_scripts/README.md
+++ treetoaster_scripts/README.md
@@ -1,1 +1,9 @@
-This repository is under construction but will eventually contain all the scripts to generate tree toaster and IVM graphs.
+
+This directory contains scripts necessary to regenerate the graphs in the Evaluation section of the paper (Figures 9-13 inclusive).
+
+Directions:
+
+Run the shell script generate_graphs_all.sh in the treetoaster_scripts directory.
+The script will download and unpack the tracefiles.
+It will then generate the graphs and place the PDFs in the treetoaster_scripts/graphs directory.
+
--- treetoaster_scripts/generate_graphs_all.sh
+++ treetoaster_scripts/generate_graphs_all.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "Downloading tracefiles"
+
+wget http://data.vizierdb.info/JITD/treetoaster_files/treetoaster_tracefiles.tgz
+result=$?
+if [ "$result" != "0" ]; then
+	echo "Error retrieving tracefiles"
+	exit 1
+fi
+
+tar -xzf treetoaster_tracefiles.tgz
+mv view_results tracefiles
+
+echo "Tracefiles downloaded"
 echo "Starting graph generation"
 
 python generate_graphs_9_10_12.py
@@ -16,5 +29,5 @@ if [ "$result" != "0" ]; then
 	exit 1
 fi
 
-echo "SUCCESS.  All graphs in ../graph directory"
+echo "\nSUCCESS.  All graphs in treetoaster_scripts/graphs directory"
 
